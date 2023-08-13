1ff4b55dda85d574cf2edd0e27b592ef225fc836
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-08-13 18:20:31 +0200

    fix bugs

--- helpers/dataValidator.js
+++ helpers/dataValidator.js
@@ -1,14 +1,10 @@
 const Joi = require("joi");
 
-const dataValidate = (data) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
-    email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-      .required(),
-    phone: Joi.string().required(),
-  });
-  return schema.validate(data);
-};
-
-module.exports = dataValidate;
+const addSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string().required(),
+});
+module.exports = { addSchema };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const router = express.Router();
-// const { validateBody } = require("../../middlewares");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../helpers/dataValidator");
 
 router.get("/", ctrl.getAll);
 
 router.get("/:contactId", ctrl.getById);
 
-router.post("/", ctrl.addOneContact);
+router.post("/", validateBody(schemas.addSchema), ctrl.addOneContact);
 
 router.delete("/:contactId", ctrl.deleteContact);
 
-router.put("/:contactId", ctrl.updateById);
+router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateById);
 
 module.exports = router;
