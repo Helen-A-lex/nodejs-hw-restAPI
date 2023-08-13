d9d17af82675d810a17696214c6b73ed6cfc2c00
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-08-13 11:53:23 +0200

    add middleware

---
+++ middlewares/validateBody.js
@@ -0,0 +1,31 @@
+const dataValidate = require("../helpers/dataValidator");
+const { HttpError } = require("../helpers");
+
+const validateBody = (dataValidate) => {
+  const func = (req, res, next) => {
+    const validateFile = dataValidate(req.body);
+    if (validateFile.error) {
+      if (Object.keys(req.body).length === 0) {
+        next(HttpError(400, `missing fields`));
+      } else {
+        const missingField = validateFile.error.details[0].path[0];
+        next(HttpError(400, `missing required ${missingField} field`));
+      }
+    }
+    next();
+  };
+  return func;
+};
+module.exports = validateBody;
+
+// const validateBody = (schema) => {
+//   const func = (req, res, next) => {
+//     const { error } = schema.validate(reg.body);
+//     if (error) {
+//       next(HttpError(400, error.message));
+//     }
+//     next();
+//   };
+//   return func;
+// };
+// module.exports = validateBody;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const router = express.Router();
+const { validateBody } = require("../../middlewares");
 
 router.get("/", ctrl.getAll);
 
 router.get("/:contactId", ctrl.getById);
 
-router.post("/", ctrl.addOneContact);
+router.post("/", validateBody, ctrl.addOneContact);
 
 router.delete("/:contactId", ctrl.deleteContact);
 
-router.put("/:contactId", ctrl.updateById);
+router.put("/:contactId", validateBody, ctrl.updateById);
 
 module.exports = router;
